f295db5fc2aa4832956894ec000111d462e5577e
isleep <goodwsj@qq.com>
2022-09-15 18:10:50 +0800

    add function: 1. network join. 2. network delete. 3. router fault. 4. display router information.
    change function:
    1. network init.

--- src/LAN.java
+++ src/LAN.java
@@ -21,6 +21,12 @@ public class LAN {
         this.routerMap = routerMap;
     }
 
+    //设置定期更新时间
+    public void setUpdateTime(Integer updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    //初始化路由器
     public void initRouter() {
         this.routerMap = new HashMap<String, Router>();
         Router routerA = new Router();
@@ -30,7 +36,7 @@ public class LAN {
         Router routerE = new Router();
         Router routerF = new Router();
 
-        //Initial Router A
+        //初始化路由器A
         Map<String, Information> informationA = new HashMap<String, Information>();
         informationA.put("Network1", new Information("Network1", 0, "null"));
         informationA.put("Network2", new Information("Network2", 0, "null"));
@@ -41,7 +47,7 @@ public class LAN {
         routerA.setAdjRouterMissCount(routerE, 0);
         this.routerMap.put("RouterA", routerA);
 
-        //Initial Router B
+        //初始化路由器B
         Map<String, Information> informationB = new HashMap<String, Information>();
         informationB.put("Network3", new Information("Network3", 0, "null"));
         informationB.put("Network4", new Information("Network4", 0, "null"));
@@ -50,7 +56,7 @@ public class LAN {
         routerB.setAdjRouterMissCount(routerC, 0);
         this.routerMap.put("RouterB", routerB);
 
-        //Initial Router C
+        //初始化路由器C
         Map<String, Information> informationC = new HashMap<String, Information>();
         informationC.put("Network4", new Information("Network4", 0, "null"));
         informationC.put("Network5", new Information("Network5", 0, "null"));
@@ -59,7 +65,7 @@ public class LAN {
         routerC.setAdjRouterMissCount(routerF, 0);
         this.routerMap.put("RouterC", routerC);
 
-        //Initial Router D
+        //初始化路由器D
         Map<String, Information> informationD = new HashMap<String, Information>();
         informationD.put("Network2", new Information("Network2", 0, "null"));
         informationD.put("Network5", new Information("Network5", 0, "null"));
@@ -69,7 +75,7 @@ public class LAN {
         routerD.setAdjRouterMissCount(routerF, 0);
         this.routerMap.put("RouterD", routerD);
 
-        //Initial Router E
+        //初始化路由器E
         Map<String, Information> informationE = new HashMap<String, Information>();
         informationE.put("Network1", new Information("Network1", 0, "null"));
         informationE.put("Network6", new Information("Network6", 0, "null"));
@@ -79,7 +85,7 @@ public class LAN {
         routerE.setAdjRouterMissCount(routerF, 0);
         this.routerMap.put("RouterE", routerE);
 
-        //Initial Router F
+        //初始化路由器F
         Map<String, Information> informationF = new HashMap<String, Information>();
         informationF.put("Network5", new Information("Network5", 0, "null"));
         informationF.put("Network6", new Information("Network6", 0, "null"));
@@ -90,44 +96,75 @@ public class LAN {
         this.routerMap.put("RouterF", routerF);
     }
 
+    //初始化网络及邻接路由器关系
     public void initNetwork() {
         this.networkMap = new HashMap<String, Network>();
 
-        //Initial Network1
+        //初始化网络1
         List<Router> adj1 = new ArrayList<Router>();
         adj1.add(routerMap.get("RouterA"));
         adj1.add(routerMap.get("RouterE"));
-        networkMap.put("Network1", new Network(adj1));
+        networkMap.put("Network1", new Network("Network1", adj1));
 
-        //Initial Network2
+        //初始化网络2
         List<Router> adj2 = new ArrayList<Router>();
         adj2.add(routerMap.get("RouterA"));
         adj2.add(routerMap.get("RouterD"));
-        networkMap.put("Network2", new Network(adj2));
+        networkMap.put("Network2", new Network("Network2", adj2));
 
-        //Initial Network3
+        //初始化网络3
         List<Router> adj3 = new ArrayList<Router>();
         adj3.add(routerMap.get("RouterA"));
         adj3.add(routerMap.get("RouterB"));
-        networkMap.put("Network3", new Network(adj3));
+        networkMap.put("Network3", new Network("Network3", adj3));
 
-        //Initial Network4
+        //初始化网络4
         List<Router> adj4 = new ArrayList<Router>();
         adj4.add(routerMap.get("RouterB"));
         adj4.add(routerMap.get("RouterC"));
-        networkMap.put("Network4", new Network(adj4));
+        networkMap.put("Network4", new Network("Network4", adj4));
 
-        //Initial Network5
+        //初始化网络5
         List<Router> adj5 = new ArrayList<Router>();
         adj5.add(routerMap.get("RouterC"));
         adj5.add(routerMap.get("RouterF"));
-        networkMap.put("Network5", new Network(adj5));
+        networkMap.put("Network5", new Network("Network5", adj5));
 
-        //Initial Network6
+        //初始化网络6
         List<Router> adj6 = new ArrayList<Router>();
         adj6.add(routerMap.get("RouterD"));
         adj6.add(routerMap.get("RouterE"));
         adj6.add(routerMap.get("RouterF"));
-        networkMap.put("Network6", new Network(adj6));
+        networkMap.put("Network6", new Network("Network6", adj6));
+    }
+
+    //网络加入
+    public void addNetwork(Network network, List<Router> adjRouterList) {
+        networkMap.put(network.getNetworkName(), new Network(network.getNetworkName(), adjRouterList));
+    }
+    
+    //网络删除
+    public void deleteNetwork(Network network) {
+        for(Router router : network.getAdjRouterList()) {
+            for(String targetNetworkName : router.getinformation().keySet()) {
+                if(targetNetworkName == network.networkName) {
+                    Information information = router.getinformation().get(targetNetworkName);
+                    information.setDistance(16);
+                    router.getinformation().put(targetNetworkName, information);
+                }
+            }
+        }
+    }
+
+    //路由器故障
+    public void routerFault(Router router) {
+        router.setRouterStatus(false);
+    }
+
+    //输出指定路由器的路由表
+    public void displayRouterInformation(Router router) {
+        for(String targetNetworkName : router.getinformation().keySet()) {
+            System.out.println(router.getinformation().get(targetNetworkName));
+        }
     }
 }
