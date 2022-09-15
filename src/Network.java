55bbfff2fc36eb75ba5b3c4b746c7c29ac219ec4
isleep <goodwsj@qq.com>
2022-09-15 18:11:18 +0800

    add member: 1. String networkName.

--- src/Network.java
+++ src/Network.java
@@ -2,14 +2,24 @@ import java.io.Serializable;
 import java.util.*;
 
 public class Network implements Serializable {
+    String networkName;
     List<Router> adjRouterList;
 
     public Network() {
 
     }
 
-    public Network(List<Router> adjRouterList) {
+    public Network(String networkName, List<Router> adjRouterList) {
+        this.networkName = networkName;
         this.adjRouterList = adjRouterList;
+    }
+
+    public String getNetworkName() {
+        return networkName;
+    }
+
+    public void setNetworkName(String networkName) {
+        this.networkName = networkName;
     }
 
     public List<Router> getAdjRouterList() {
