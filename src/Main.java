bf20be6e409d3955908ea9b6bb8df9eac03b114c
isleep <goodwsj@qq.com>
2022-09-26 01:16:52 +0800

    Add Swing in Main.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,13 +9,13 @@ public class Main {
         LAN lan = new LAN();
         lan.initRouter();
         lan.initNetwork();
-        List<Thread> list = new ArrayList<>(6);
-
-        int index = 0;
         for(String routerName : lan.getRouterMap().keySet()) {
-            Thread thread = list.get(index++);
-            thread = lan.getRouterMap().get(routerName);
-            thread.start();
+            lan.getRouterMap().get(routerName).start();
         }
+
+        JFrame jf = new JFrame("RIP协议模拟");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setSize(800, 600);
+        jf.setVisible(true);
     }
 }
